352604f7a0afbd06e03c152300f50ee0610cf527
mimishkin <mishmark.home@ya.ru>
2024-11-04 02:16:40 +0300

    mod table

---
+++ app/Http/Controllers/SimCardsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SimCards;
+use Illuminate\Http\Request;
+
+class SimCardsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SimCards $simCards)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SimCards $simCards)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SimCards $simCards)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SimCards $simCards)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_11_03_231612_create_sim_cards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sim_cards', function (Blueprint $table) {
+            $table->unsignedInteger('IMEI');
+            $table->timestamps('date_create');
+            $table->unsignedInteger('number');
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('plan_id')->constrained();
+            $table->boolean('visible');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sim_cards');
+    }
+};
